fa0a098239ba3f38e946aecc006a6166a7e3abba
chen-jingfan <12354414+chen-jingfan@user.noreply.gitee.com>
2025-06-21 22:17:13 +0800

    优化弹出

--- projects/project/src/lib/core/animation/modal.animation.ts
+++ projects/project/src/lib/core/animation/modal.animation.ts
@@ -3,11 +3,10 @@ import { trigger, state, style, transition, animate } from "@angular/animations"
 export const modalAnimation = trigger('modalAnimation', [
     state('void', style({
       opacity: 0,
-      height: '0px',
+      transform: 'scale(0.4)'
     })),
     state('visible', style({
       opacity: 1,
-      height: '*',
     })),
     transition('void => visible', animate('150ms cubic-bezier(0, 0, 0.2, 1)')),
     transition('visible => void', animate('150ms cubic-bezier(0.4, 0, 0.2, 1)'))
